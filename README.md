55d7be78d6a8ff44f42bc2f6b53f4d0c243b3841
stone0090 <stone0090@qq.com>
2021-08-07 14:31:19 +0800

    feat：集成druid数据库连接池及监控页面

--- README.md
+++ README.md
@@ -1,26 +1,31 @@
 # 工程简介
 
 ## 技术选型
-
 - 脚手架：[Aliyun Java Initializr](https://start.aliyun.com/bootstrap.html)
 - 核心框架：[Spring Boot](https://spring.io/projects/spring-boot)
 - 数据库层：[MyBatis 3](https://mybatis.org/mybatis-3/zh/index.html)
-- 数据库连接池：[Druid](https://github.com/alibaba/druid)
 - 数据库：[h2database](http://www.h2database.com/html/main.html)
-- 缓存：[Ehcache](https://www.ehcache.org/)
+- 连接池：[Druid](https://github.com/alibaba/druid)
 - 安全框架：[Apache Shiro](http://shiro.apache.org/)
 - 前端框架：[Ant Design Pro](https://pro.ant.design)
 - 接口文档：[swagger](https://swagger.io/)
 - 诊断工具：[Arthas](https://arthas.aliyun.com/doc/)
 
+## 访问入口
+- 系统首页：
+  - 访问入口：http://localhost:8000
+  - 账号密码：stone / 123456
+- h2数据库：
+  - 访问入口：http://127.0.0.1:8080/h2-console
+  - 账号密码：san /（没有密码）
+  - jdbcurl：jdbc:h2:file:./demo-dao/src/main/resources/database/demo
+- druid监控：
+  - 访问入口：http://localhost:8080/druid/index.html
+  - 账号密码：stone / 123456
+  
 ## 功能设计（初步设想）
-- 部门管理
 - 用户管理
 - 角色管理
-- 菜单管理
-- 权限分配
-- 参数管理
-- 数据字典
-- 定时任务
-- 业务日志
-- 登录日志
+- 权限管理
+
+## 日志规范
---
+++ demo-web/src/main/java/com/example/demo/web/config/DruidConfig.java
@@ -0,0 +1,50 @@
+package com.example.demo.web.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author stone
+ * @date 2021/08/07
+ */
+@Configuration
+public class DruidConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DruidDataSource druidDataSource() {
+        return new DruidDataSource();
+    }
+
+    @Bean
+    public ServletRegistrationBean druidStatViewServlet() {
+        ServletRegistrationBean<StatViewServlet> registrationBean = new ServletRegistrationBean<>(new StatViewServlet(), "/druid/*");
+        registrationBean.addInitParameter("allow", ""); // IP白名单 (没有配置或者为空，则允许所有访问)
+        registrationBean.addInitParameter("deny", "");  // IP黑名单 (共同存在时，deny优先于allow)
+        registrationBean.addInitParameter("loginUsername", "stone");
+        registrationBean.addInitParameter("loginPassword", "123456");
+        registrationBean.addInitParameter("resetEnable", "false");
+        return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean druidWebStatFilter() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
+        Map initParams = new HashMap<>();
+        initParams.put("exclusions", "*.js,*.css,/druid/*");
+        filterRegistrationBean.setInitParameters(initParams);
+        filterRegistrationBean.setUrlPatterns(Arrays.asList("/*"));
+        return filterRegistrationBean;
+    }
+}
