38a425d1171cd9f19e0bad709ed7368955294f56
stone0090 <stone0090@126.com>
2023-06-01 00:08:15 +0800

    refactor：配置优化。

--- demo-web/src/main/java/com/example/demo/web/config/DruidConfig.java
+++ demo-web/src/main/java/com/example/demo/web/config/DruidConfig.java
@@ -1,18 +1,10 @@
 package com.example.demo.web.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
-import com.alibaba.druid.support.http.StatViewServlet;
-import com.alibaba.druid.support.http.WebStatFilter;
-
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author stone
@@ -22,29 +14,9 @@ import java.util.Map;
 public class DruidConfig {
 
     @Bean
-    @ConfigurationProperties(prefix = "spring.datasource")
+    @ConfigurationProperties(prefix = "spring.datasource.druid")
     public DruidDataSource druidDataSource() {
-        return new DruidDataSource();
+        return DruidDataSourceBuilder.create().build();
     }
 
-    @Bean
-    public ServletRegistrationBean druidStatViewServlet() {
-        ServletRegistrationBean<StatViewServlet> registrationBean = new ServletRegistrationBean<>(new StatViewServlet(), "/druid/*");
-        registrationBean.addInitParameter("allow", ""); // IP白名单 (没有配置或者为空，则允许所有访问)
-        registrationBean.addInitParameter("deny", "");  // IP黑名单 (共同存在时，deny优先于allow)
-        registrationBean.addInitParameter("loginUsername", "stone");
-        registrationBean.addInitParameter("loginPassword", "123456");
-        registrationBean.addInitParameter("resetEnable", "false");
-        return registrationBean;
-    }
-
-    @Bean
-    public FilterRegistrationBean druidWebStatFilter() {
-        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
-        Map initParams = new HashMap<>();
-        initParams.put("exclusions", "*.js,*.css,/druid/*");
-        filterRegistrationBean.setInitParameters(initParams);
-        filterRegistrationBean.setUrlPatterns(Arrays.asList("/*"));
-        return filterRegistrationBean;
-    }
 }
